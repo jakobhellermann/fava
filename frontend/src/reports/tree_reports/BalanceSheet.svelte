3d078c1c6a62587ec40578d794ed16d7bb5c41a6
Jakob Hellermann <jakob.hellermann@protonmail.com>
2026-03-15 18:08:34 +0100

    jj: balance sheet: remove empty tables (e.g. liabilities)

--- frontend/src/reports/tree_reports/BalanceSheet.svelte
+++ frontend/src/reports/tree_reports/BalanceSheet.svelte
@@ -1,22 +1,26 @@
 <script lang="ts">
   import ChartSwitcher from "../../charts/ChartSwitcher.svelte";
+  import { is_empty } from "../../lib/objects.ts";
   import TreeTable from "../../tree-table/TreeTable.svelte";
   import type { TreeReportProps } from "./index.ts";
 
   let { charts, trees, date_range }: TreeReportProps = $props();
   let end = $derived(date_range?.end ?? null);
+  let non_empty_trees = $derived(
+    trees.filter((tree) => !is_empty(tree.balance_children)),
+  );
 </script>
 
 <ChartSwitcher {charts} />
 
 <div class="row">
   <div class="column">
-    {#each trees.slice(0, 1) as tree (tree.account)}
+    {#each non_empty_trees.slice(0, 1) as tree (tree.account)}
       <TreeTable {tree} {end} />
     {/each}
   </div>
   <div class="column">
-    {#each trees.slice(1) as tree (tree.account)}
+    {#each non_empty_trees.slice(1) as tree (tree.account)}
       <TreeTable {tree} {end} />
     {/each}
   </div>
